5e2d36d8820717055a4aadf54e1876a977140b33
Edwin Islas Aguirre <8241001@isd535.org>
2022-02-07 11:32:16 -0600

    added basics, will add more later

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-// Put your JavaScript here
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(232, 152, 12, 0, Math.PI*2, false);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.closePath();
