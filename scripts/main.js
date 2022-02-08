7838f912b1f2de03c72601f5214d686afdaa4bae
Edwin Islas Aguirre <8241001@isd535.org>
2022-02-07 23:35:14 -0600

    new changes, not done whatsoever

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,30 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(232, 152, 12, 0, Math.PI*2, false);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
+let x = canvas.width/2;
+let y = canvas.height-30;
+
+let dx = 2;
+let dy = -2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI*2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    // clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // draw the ball
+    drawBall();
+    
+    // change the x and the y values for the ball
+    x+= dx;
+    y+= dy;
+
+}
+setInterval(draw, 10);
