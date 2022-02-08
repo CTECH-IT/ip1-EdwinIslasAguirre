ca0119ae56004d28b47cd3e6e85b809ad1cce8ef
Edwin Islas Aguirre <8241001@isd535.org>
2022-02-08 11:31:24 -0600

    not nearly done

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,26 @@ let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
 
+let ballRadius = 15;
+
+let paddleHeight = 20;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth) /2;
+
+let rightPressed = false;
+let leftPressed = false;
+
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 20, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
@@ -26,5 +43,52 @@ function draw() {
     x+= dx;
     y+= dy;
 
+    // check to see if we've gone off the edge
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+
+    // paddle controls
+    if(rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed){
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
+
+    drawPaddle();
+
 }
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 setInterval(draw, 10);
