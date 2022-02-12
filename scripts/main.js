e48060d234525e585600a8571d0981c4605315c1
Edwin Islas Aguirre <8241001@isd535.org>
2022-02-11 19:16:31 -0600

    done with game, finally

--- scripts/main.js
+++ scripts/main.js
@@ -1,106 +1,132 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
-let x = canvas.width/2;
-let y = canvas.height-30;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 
 let dx = 2;
 let dy = -2;
 
-let ballRadius = 10
+let ballRadius = 15;
 
 let paddleHeight = 10;
-let paddleWidth = 75;
-let paddleX = (canvas.width-paddleWidth) /2;
+let paddleWidth = 95;
+let paddleX = (canvas.width - paddleWidth) / 2;
 
 let rightPressed = false;
-let leftPressed = false; 
+let leftPressed = false;
 
-function drawBall(){
+let score = 0;
+
+function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
 }
 
-let score = 0;
-
-function drawPaddle(){
+function drawPaddle() {
     ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD"
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
-    // clear the canvas
+    // clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     // draw the ball
     drawBall();
 
-    // change the x and y values for the ball
+    // changes x and y values
     x += dx;
     y += dy;
 
-    // check to see if we've gotten off the edge of the board
-    if (x  + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    // check to see if you have hit the edge
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if (y + dy < ballRadius) { //ceiling check
+    if (y + dy < ballRadius) {  // ceiling check
         dy = -dy;
-    } else if (y + dy > canvas.height-ballRadius) { // floor check
-        if(x > paddleX && x < paddleX + paddleWidth) { // paddle check
+    } else if (y + dy > canvas.height - ballRadius) {  // floor check
+        if (x > paddleX && x < paddleX + paddleWidth) {  // paddle check
             dy = -dy;
-        } else { // it hit the floor!
-            alert("GAME OVER, YOU TOUCHED THE LAVA");
+            score += 1;
+            dx += 1
+            dy += -1
+        }
+        else {  // it hit the floor!
+            alert("YOU TOUCHED THE LAVA, GAME OVER :(");
             document.location.reload();
-           learInterval(interval); // needed for browser to end game
-        }    
+            clearInterval(interval);  // needed for the browser to end the game
+        }
     }
 
-
-   
     // paddle controls
-    if(rightPressed) {
-         paddleX += 7;
-         if (paddleX +  paddleWidth > canvas.width){
-             paddleX = canvas.width - paddleWidth;
-         }
+    if (rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
     }
-    else if(leftPressed) { 
-         paddleX -=7;
-         if (paddleX < 0){
-             paddleX = 0;
-         }
-    }    
+    else if (leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0) {
+            paddleX = 0;
+        }
+    }
 
     drawPaddle();
 
-} 
+    drawScore();
+
+    drawSpeed();
+
+}
 
 function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
+    if (e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = true;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = true;
     }
 }
 
-function keyUpHandler(e){
-    if(e.key == "Right" || e.key == "ArrowRight") {
+function drawScore() {
+    ctx.font = "16px Open Sans";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Score: " + score, 8, 310);
+}
+
+function drawSpeed() {
+    ctx.font = "16px Open Sans";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Speed: " + Math.abs(dy), 405, 310);
+}
+
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = false;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
+    }
+}
+
+function mouseMoveHandler(e) {
+    let relativeX = e.cilentX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/ 2;
     }
 }
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 let interval = setInterval(draw, 10);
